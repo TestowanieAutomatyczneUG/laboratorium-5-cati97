8ca61a47818519918a62bd861396e59431cdb30b
unknown <katasiw97@gmail.com>
2020-11-02 19:41:36 +0100

    test_disallow_first_strand_longer - green

--- src/Hamming.py
+++ src/Hamming.py
@@ -1,14 +1,17 @@
 class Hamming:
     def distance(self, first, second):
-        if first == "" and second == "":
-            return 0
-        elif first == second:
-            return 0
+        if len(first) > len(second):
+            raise ValueError("First strand cannot be longer!")
         else:
-            countDiff = 0
-            for i in range(len(first)):
-                if first[i] != second[i]:
-                    countDiff += 1
-                else:
-                    continue
-            return countDiff
+            if first == "" and second == "":
+                return 0
+            elif first == second:
+                return 0
+            else:
+                countDiff = 0
+                for i in range(len(first)):
+                    if first[i] != second[i]:
+                        countDiff += 1
+                    else:
+                        continue
+                return countDiff

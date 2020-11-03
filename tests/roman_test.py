dd67ee1b2cc1b2a98a879fb7ad552e1bd858d768
unknown <katasiw97@gmail.com>
2020-11-03 11:16:24 +0100

    Refactored code

--- tests/roman_test.py
+++ tests/roman_test.py
@@ -1,44 +1,25 @@
 import unittest
 
+
 def roman(number):
-    if number == 1:
-        return "I"
-    elif number == 2:
-        return "II"
-    elif number == 3:
-        return "III"
-    elif number == 4:
-        return "IV"
-    elif number == 5:
-        return "V"
-    elif number == 6:
-        return "VI"
-    elif number == 9:
-        return "IX"
-    elif number == 27:
-        return "XXVII"
-    elif number == 48:
-        return "XLVIII"
-    elif number == 49:
-        return "XLIX"
-    elif number == 59:
-        return "LIX"
-    elif number == 93:
-        return "XCIII"
-    elif number == 141:
-        return "CXLI"
-    elif number == 163:
-        return "CLXIII"
-    elif number == 402:
-        return "CDII"
-    elif number == 575:
-        return "DLXXV"
-    elif number == 911:
-        return "CMXI"
-    elif number == 1024:
-        return "MXXIV"
-    elif number == 3000:
-        return "MMM"
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1]
+    syb = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"]
+    roman_num = ''
+    i = 0
+    while number > 0:
+        for _ in range(number // val[i]):
+            roman_num += syb[i]
+            number -= val[i]
+        i += 1
+    return roman_num
 
 
 class RomanNumeralsTest(unittest.TestCase):
